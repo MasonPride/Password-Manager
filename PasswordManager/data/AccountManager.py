5d32dd615c35b6f1e708f3cbfca656b305313c22
Mason Pride <mtpride07@gmail.com>
2024-11-05 14:42:58 -0600

    Added documentation

--- PasswordManager/data/AccountManager.py
+++ PasswordManager/data/AccountManager.py
@@ -1,10 +1,10 @@
 """Account Manager class.
 
 Creates and manages the accounts by using a 
-List[Account]
+List[Account].
 
 Author: Mason Pride
-Version: 0.1
+Date: 10/29/24
 """
 from typing import List
 from PasswordManager.data.Account import Account
@@ -12,29 +12,49 @@ from PasswordManager.data.FileHandler import FileHandler
 
 
 class AccountManager():
-    """Account Manager class."""
+    """Account Manager class.
+    
+    Creates the account manager, in charge
+    reading the conetent from AccountInfo.txt,
+    and creating a list of all accounts
+    """
     
     def __init__(self) -> None:
+        """Constructor for AccountManager.
+        
+        Creates the account list, file handler,
+        and reads content from the file.
+        """
         self.__account_list: List[Account] = []
-        self.__file_handler = FileHandler("AccountInfo.txt", b'ize-EjWncqijjNMgJsdhuHxm3o5xC4W1tZ9MRAH2Uog=')
+        self.__file_handler = FileHandler("AccountInfoTest.txt", b'ize-EjWncqijjNMgJsdhuHxm3o5xC4W1tZ9MRAH2Uog=')
         content = self.__file_handler.read()
-        content = content.split("\n")
-        for account in content:
-            account_split = account.split(" ")
-            self.__account_list.append(Account(account_split[0], account_split[1], account_split[2]))
+        if content:
+            content = content.split("\n")
+            for account in content:
+                account_split = account.split(" ")
+                self.__account_list.append(Account(account_split[0], account_split[1], account_split[2]))
 
     @property
     def account_list(self) -> List[Account]:
-        """Getter for account list attribute"""
+        """Getter for account list attribute.
+        
+        Returns:
+            List[Account] representing the account list"""
         return self.__account_list
 
     def add_account(self, account: Account) -> None:
-        """Adds account to account list"""
+        """Adds account to account list.
+        
+        Adds a desired account to the account list.
+        
+        Args:
+            account: Account being added to list.
+        """
         self.__account_list.append(account)
         self.__file_handler.write(str(account))
 
     def __str__(self) -> str:
-        """String override method"""
+        """String override method."""
         output = ""
         for account in self.__account_list:
             output += str(account)
